0a2dd6b3421cabbe6e9fe71ab5948b3761a96671
lidong <642540749@qq.com>
2019-01-15 14:11:02 +0800

    #标大大分享  域名得更改

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/proxy': {
+        target: 'http://pre.biaodaa.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/proxy': ''
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
