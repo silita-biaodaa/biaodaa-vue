edb9a14935a884ed8c3061d7cd710d0ab8d0bd52
lidong <642540749@qq.com>
2019-02-19 09:09:52 +0800

    #标大大 DEBUG

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 // const baseURL = 'http://192.168.1.161:8080/biaodaa-back/'
 // const baseURL = 'http://192.168.1.131:8080/'
-const baseURL = 'http://api.biaodaa.com/'
+// const baseURL = 'http://api.biaodaa.com/.'
 // const baseURL = 'http://120.78.253.169/'
-// const baseURL = 'http://pre.biaodaa.com'
+const baseURL = 'http://pre.biaodaa.com'
 
 
 axios.defaults.baseURL = baseURL
